a7d0187417fbbd2919ca7648359f7bc988786936
vastopol <classicunix@gmail.com>
2016-07-31 21:53:24 +0000

    structure and style

--- Assembly/asm_files/fib.asm
+++ Assembly/asm_files/fib.asm
@@ -1,4 +1,3 @@
-;TEST ASSEMBLY PROGRAM #2
 ;Fibonacci Sequence
 ;========================
 
@@ -6,39 +5,39 @@
 
 ;INSTRUCTIONS
 ;--------------------
-  LEA R0, wrd_1
-  PUTS			        ; TRAP x22; print R0 to screen
+LEA R0, wrd_1
+PUTS			        ; TRAP x22; print R0 to screen
+
+LD R1, num_1
+LD R2, num_2
+LD R3, #0         ; used as variable to sum sequence up to n
+LD R4, index
+
+LOOP_START
+  ADD R3, R1, R2  ; R3 = R1 + R2
   
-  LD R1, num_1
-  LD R2, num_2
-  LD R3, #0         ; used as variable to sum sequence up to n
-  LD R4, index
+  AND R1, R1, #0	
+  ADD R1, R2, #0	; set R1 = R2
+  
+  AND R2, R2, #0	
+  ADD R2, R3, #0	; set R2 = R3
 
-  LOOP_START
-    ADD R3, R1, R2  ; R3 = R1 + R2
-    
-    AND R1, R1, #0	
-    ADD R1, R2, #0	; set R1 = R2
-    
-    AND R2, R2, #0	
-    ADD R2, R3, #0	; set R2 = R3
+  ADD R4, R4, #-1	; index--
+BRp LOOP_START
 
-    ADD R4, R4, #-1	; index--
-  BRp LOOP_START
+LEA R0, wrd_2
+PUTS
 
-  LEA R0, wrd_2
-  PUTS
-
-  HALT
+HALT
 
 ;DATA
 ;-------------------------
-  num_1 	.FILL #0	; R1  0
-  num_2 	.FILL #1	; R2  1
-  index 	.FILL #7	; R4  index of nth fibonacci #
+num_1 	.FILL #0	; R1  0
+num_2 	.FILL #1	; R2  1
+index 	.FILL #7	; R4  index of nth fibonacci #
 
-  wrd_1 .STRINGZ "START\n"
-  wrd_2 .STRINGZ "END\n" 
+wrd_1 .STRINGZ "START\n"
+wrd_2 .STRINGZ "END\n" 
 
 .END
 
--- Assembly/asm_files/multiply.asm
+++ Assembly/asm_files/multiply.asm
@@ -1,4 +1,4 @@
-;TEST ASSEMBLY
+;Multiplication
 ;(a*b)
 ;========================
 
@@ -6,20 +6,20 @@
 
 ;INSTRUCTIONS
 ;--------------------
-  LD R1, num_1
-  LD R2, num_2
-  ADD R4, R2, #0	  ; index, copy b to use as decrement index with a "loop"
+LD R1, num_1
+LD R2, num_2
+ADD R4, R2, #0	  ; index, copy b to use as decrement index with a "loop"
 
-  LOOP_START
-    ADD R3, R3, R1
-    ADD R4, R4, #-1 ; index-- 
-  BRp LOOP_START    ; conditional branch to LOOP_START label if R4 contains a positive value
+LOOP_START
+  ADD R3, R3, R1
+  ADD R4, R4, #-1 ; index-- 
+BRp LOOP_START    ; conditional branch to LOOP_START label if R4 contains a positive value
 
-  HALT
+HALT
 
 ;DATA
 ;--------------------
-  num_1 .FILL #4		; a
-  num_2 .FILL #5		; b
+num_1 .FILL #4		; a
+num_2 .FILL #5		; b
 
 .END
