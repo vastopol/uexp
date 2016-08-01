e3d7495c79ecd62170cf62e1d9b6caeb005fc004
vastopol <classicunix@gmail.com>
2016-08-01 16:27:08 +0000

    fix whitespace

--- Assembly/asm_files/nand_logic/logic.asm
+++ Assembly/asm_files/nand_logic/logic.asm
@@ -9,7 +9,7 @@
 
 LD R1, var_A	;VALUE
 LD R2, var_B	;VALUE
-LD R6, ptr	;ADDRESS
+LD R6, ptr	    ;ADDRESS
 
 STR R1, R6, #0	; store: A
 STR R2, R6, #1	; store: B
@@ -19,10 +19,10 @@ AND R3, R1, R2
 STR R3, R6, #2	; store: A and B
 
 ;OR
-NOT R4, R1	; not(A)
-NOT R5, R2	; not(B)
+NOT R4, R1	    ; not(A)
+NOT R5, R2	    ; not(B)
 AND R3, R4, R5	; not(A) and not(B)
-NOT R3, R3	; not( not(A) and not(B) )
+NOT R3, R3	    ; not( not(A) and not(B) )
 STR R3, R6, #3	; store: A or B
 
 
