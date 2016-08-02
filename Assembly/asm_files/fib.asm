54b7e4ea6d35ea4c1637cdd80e065757c0f05da1
vastopol <classicunix@gmail.com>
2016-08-02 19:36:20 +0000

    experiment with tab vs space

--- Assembly/asm_files/fib.asm
+++ Assembly/asm_files/fib.asm
@@ -6,7 +6,7 @@
 ;INSTRUCTIONS
 ;--------------------
 LEA R0, wrd_1
-PUTS			        ; TRAP x22; print R0 to screen
+PUTS              ; TRAP x22; print R0 to screen
 
 LD R1, num_1
 LD R2, num_2
@@ -17,12 +17,12 @@ LOOP_START
   ADD R3, R1, R2  ; R3 = R1 + R2
   
   AND R1, R1, #0	
-  ADD R1, R2, #0	; set R1 = R2
+  ADD R1, R2, #0  ; set R1 = R2
   
   AND R2, R2, #0	
-  ADD R2, R3, #0	; set R2 = R3
+  ADD R2, R3, #0  ; set R2 = R3
 
-  ADD R4, R4, #-1	; index--
+  ADD R4, R4, #-1 ; index--
 BRp LOOP_START
 
 LEA R0, wrd_2
@@ -32,9 +32,9 @@ HALT
 
 ;DATA
 ;-------------------------
-num_1 	.FILL #0	; R1  0
-num_2 	.FILL #1	; R2  1
-index 	.FILL #7	; R4  index of nth fibonacci #
+num_1 	.FILL #0  ; R1  0
+num_2 	.FILL #1  ; R2  1
+index 	.FILL #7  ; R4  index of nth fibonacci #
 
 wrd_1 .STRINGZ "START\n"
 wrd_2 .STRINGZ "END\n" 
