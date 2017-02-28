a621e3a7338e43fa2f657f970005b443c54524ec
vastopol <classicunix@gmail.com>
2017-02-28 22:06:02 +0000

    remove attempted xlisp build, saved tar file if ever resume later

--- Lisp/XLisp/xlisp_3/fact.lsp
+++
@@ -1,5 +0,0 @@
-(define (fact n)
-  (let loop ((n n))
-    (if (= n 1)
-      1
-      (* n (loop (- n 1))))))
--- Lisp/XLisp/xlisp_3/xlispw/tclicontobitmap.h
+++
@@ -1,8 +0,0 @@
-//
-// TclIconToBitmap.h
-//
-
-#ifdef __cplusplus
-extern "C" 
-#endif
-HBITMAP _cdecl TclIconToBitmap(HDC hdc, char *pData, DWORD *pdwWidth, DWORD *pdwHeight);
